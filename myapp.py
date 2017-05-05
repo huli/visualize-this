b8ed012696b8d5de17ee9280c96d68d48b1a75eb
Huli <huli@eitschei.com>
2017-05-05 20:40:14 +0200

    Typo.

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 def app(environ, start_response):
-        data = b"Hello, Wonderwoman!\n"
+        data = b"Take it easy, Wonderwoman!\n"
         start_response("200 OK", [
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(data)))
