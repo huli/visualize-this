50619fc1ada2e96288eefe1aaca135f8a01a608c
Huli <huli@eitschei.com>
2017-05-05 19:55:42 +0200

    Another try.

--- myapp.py
+++ myapp.py
@@ -1,7 +1,4 @@
+from django.shortcuts import render_to_response
+
 def app(environ, start_response):
-        data = b"Hello, World!\n"
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
+   return render_to_response('sometemplate.html')
